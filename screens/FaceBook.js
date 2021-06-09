0f9f6dda0d6b9ae971ca3f16ccd56e91d06630d0
sachin0078 <68186774+sachin0078@users.noreply.github.com>
2021-06-09 17:09:35 +0530

    Add files via upload

---
+++ screens/FaceBook.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class FaceBook extends React.Component{
+    render(){
+        return(
+            <Text>FaceBook</Text>
+        )
+    }
+}
